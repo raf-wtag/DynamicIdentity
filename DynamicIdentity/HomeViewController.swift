5e3bf7fd7e711a79459a3dc49ad26a07472adb15
raf-wtag <fahim.rahman@welldev.io>
2023-08-03 16:42:44 +0600

    Alternate icons added

--- DynamicIdentity/HomeViewController.swift
+++ DynamicIdentity/HomeViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    @IBOutlet var currentAppIconName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currentAppIconName.text = UIApplication.shared.alternateIconName
     }
-
-
+    
+    
+    @IBAction func setPrimaryIconDidTap(_ sender: Any) {
+        setIcon(nil)
+    }
+    
+    @IBAction func setAlternateIcon1DidTap(_ sender: Any) {
+        setIcon("AlternateIcon1")
+    }
+    
+    private func setIcon(_ title: String?) {
+        UIApplication.shared.setAlternateIconName(title) { error in
+            if let error {
+                self.displayErrorAlert(with: error.localizedDescription)
+            } else {
+                print("System will display an alert")
+            }
+        }
+    }
+    
+    private func displayErrorAlert(with errorMessage: String) {
+        let alert = UIAlertController(title: "Icon Update Failed", message: errorMessage, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        alert.addAction(cancelAction)
+        self.present(alert, animated: false)
+    }
 }
 
