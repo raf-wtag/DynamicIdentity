eb4697c07f454f4714d8b51bb9cdf40b58426d2e
raf-wtag <fahim.rahman@welldev.io>
2023-08-06 13:15:38 +0600

    Alternate app icon silently change implemented
    - Added additional button in storyboard
    
    - Added functionality to that button to change the app icon silently

--- DynamicIdentity/HomeViewController.swift
+++ DynamicIdentity/HomeViewController.swift
@@ -25,12 +25,36 @@ class HomeViewController: UIViewController {
         setIcon("AlternateIcon1")
     }
     
+    @IBAction func setAlernateIconSilentlyDidTap(_ sender: Any) {
+        setIconSilently("AlternateIcon1")
+    }
+    
     private func setIcon(_ title: String?) {
         UIApplication.shared.setAlternateIconName(title) { error in
             if let error {
                 self.displayErrorAlert(with: error.localizedDescription)
             } else {
                 print("System will display an alert")
+            }
+        }
+    }
+    
+    private func setIconSilently(_ iconName: String?) {
+        guard let iconName else { return }
+        
+        // anti apple private method call analyze
+        if UIApplication.shared.responds(to: Selector(("supportsAlternateIcons"))),
+           UIApplication.shared.supportsAlternateIcons {
+            
+            var selectorString = "_setAlternate"
+            selectorString += "IconName:"
+            selectorString += "completionHandler:"
+            
+            let selector = Selector(selectorString)
+            if let imp = UIApplication.shared.method(for: selector) {
+                typealias LCSetAlternateIconNameFunc = @convention(c) (NSObject, Selector, String, @escaping (Error?) -> Void) -> Void
+                let funcPtr = unsafeBitCast(imp, to: LCSetAlternateIconNameFunc.self)
+                funcPtr(UIApplication.shared, selector, iconName) { _ in }
             }
         }
     }
